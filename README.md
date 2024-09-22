5eb1bd2ec3d75d26d5549228571e66fdf0c35c3f
omatheusfeitosa <matheusfeitosaalcantara@gmail.com>
2024-09-22 14:42:11 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Portfólio - Matheus Feitosa
+
+Este é um projeto de portfólio pessoal desenvolvido para apresentar minhas habilidades e experiências em desenvolvimento web. O portfólio inclui uma breve introdução sobre mim, minhas habilidades, hobbies, formação acadêmica e experiência profissional. O formulário de contato integrado permite que os visitantes entrem em contato diretamente comigo via e-mail.
+
+## Funcionalidades
+
+- **Scroll Reveal**: Efeitos de rolagem que revelam elementos da página suavemente.
+- **Formulário de Contato**: Envio de mensagens via EmailJS.
+- **Experiência Profissional**: Projetos com links para repositórios e demos.
+- **Layout Responsivo**: Adaptação a diferentes tamanhos de tela com uso de media queries.
+
+## Tecnologias Utilizadas
+
+- **HTML5**
+- **CSS3**: Com o uso de módulos CSS para facilitar a manutenção e adicionar responsividade através de media queries.
+- **JavaScript**: Para interatividade, incluindo integração com EmailJS e ScrollReveal.
+- **EmailJS**: Integração para envio de e-mails via formulário de contato.
+- **Font Awesome**: Ícones para enriquecer a interface.
+- **ScrollReveal.js**: Animação ao revelar elementos ao rolar a página.
+
+## Estrutura de Pastas
+
+- **/assets**: Contém imagens e ícones utilizados no site.
+- **/styles**: Contém o arquivo CSS principal `style.css`.
+- **/JS**: Contém o script principal `main.js`.
+
+## Variáveis de Ambiente
+
+Foram adicionadas variáveis de ambiente para garantir maior segurança nas chaves sensíveis, como as do EmailJS.
+
+As seguintes variáveis estão configuradas:
+
+- `EMAILJS_SERVICE_ID`
+- `EMAILJS_TEMPLATE_ID`
+- `EMAILJS_KEY_ID`
---
+++ assets/JS/main.js
@@ -0,0 +1,93 @@
+require('dotenv').config();
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const templateParams = {
+            user_name: document.querySelector('#nome').value,
+            user_email: document.querySelector('#email').value,
+            subject: document.querySelector('#assunto').value,
+            message: document.querySelector('#mensagem').value,
+            to_email: 'matheusfeitosaalcantara@gmail.com'
+        };
+
+        emailjs.send(process.env.EMAILJS_SERVICE_ID, process.env.EMAILJS_TEMPLATE_ID, templateParams)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Mensagem enviada com sucesso!');
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert('Ocorreu um erro ao enviar a mensagem. Tente novamente.');
+            });
+    });
+});
+
+
+ScrollReveal().reveal('.principal-title, .principal-img, .about-me', {
+    duration: 1000, 
+    origin: 'bottom',
+    distance: '50px', 
+    reset: false,
+    easing: 'ease',
+  });
+
+ScrollReveal().reveal('#principal-item1, #principal-item2, #principal-item3, #principal-item4', {
+    duration: 1000, 
+    origin: 'bottom',
+    distance: '50px', 
+    reset: false,
+    delay: 500,
+    interval: 500
+});
+
+ScrollReveal().reveal('#skill-item1, #skill-item2, #skill-item3, #skill-item4, #skill-item5', {
+    duration: 1000, 
+    origin: 'left',
+    distance: '50px', 
+    reset: false,
+    delay: 500,
+    interval: 500,
+});
+
+ScrollReveal().reveal('#hobbie-item1, #hobbie-item2, #hobbie-item3, #hobbie-item4, #hobbie-item5', {
+    duration: 1000, 
+    origin: 'right',
+    distance: '50px', 
+    reset: false,
+    delay: 500,
+    interval: 500
+});
+
+
+ScrollReveal().reveal('#formacao-element2', {
+    duration: 1500, 
+    origin: 'bottom',
+    distance: '50px', 
+    reset: true,
+});
+
+ScrollReveal().reveal('#formacao-element1, #formacao-element3', {
+    duration: 3000, 
+    origin: 'bottom',
+    distance: '50px', 
+    reset: false,
+});
+
+ScrollReveal().reveal('.experiencia-item1', {
+    duration: 1500, 
+    origin: 'left',
+    distance: '50px', 
+    reset: false,
+});
+
+ScrollReveal().reveal('.experiencia-item2', {
+    duration: 3000, 
+    origin: 'right',
+    distance: '50px', 
+    reset: false,
+});
+
+
+
+  
